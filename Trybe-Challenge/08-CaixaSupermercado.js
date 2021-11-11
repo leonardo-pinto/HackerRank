17dd6380e2f65f0a17282ee0032003999ddd3401
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-11-11 20:18:59 -0300

    caixa supermercado test

--- Trybe-Challenge/08-CaixaSupermercado.js
+++ Trybe-Challenge/08-CaixaSupermercado.js
@@ -1,15 +1,15 @@
 function caixa(values) {
   let newArr = values;
+
   for (let i = 0; i <= values.length; i++) {
     if (values[i] === 0) {
-      console.log(newArr)
-      newArr.splice(i-1, 2);
-      console.log(newArr)
+      newArr.splice(i - 1, 2);
+      i = i-2
     }
-    
   }
 
-  return newArr;
+  let sumNewArr = newArr.reduce((acc, curr) => acc + curr, 0)
+  return sumNewArr;
 }
 
 const values = [1, 3, 5, 4, 0, 0, 7, 0, 0, 6]
